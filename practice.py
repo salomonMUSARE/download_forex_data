69f5c66800fcc32546eedf56ac15eabc4668a878
salomonMUSARE <salomonmusare44@gmail.com>
2021-10-12 15:03:21 +0000

    first commit

--- practice.py
+++ practice.py
@@ -531,7 +531,7 @@ def test_agent(test_df, test_df_nomalized, visualize=False, test_episodes=10, fo
 
 
 if __name__ == "__main__":            
-    df = pd.read_csv('./1h_btcusd.csv')
+    df = pd.read_csv('./BTCUSD_1h_new.csv')
     # df=df[['Date', 'Open', 'Close', 'High', 'Low', 'Volume']]
     # df = df.sort_values('Date')
     df = AddIndicators(df) 
@@ -565,20 +565,22 @@ if __name__ == "__main__":
     
     # split training and testing datasets
     train_df = df[:-test_window-lookback_window_size] # we leave 100 to have properly calculated indicators
-    test_df = df[-test_window-lookback_window_size:]
+    test_df = df[-test_window-lookback_window_size:-720*2]
+    print("#===================================================================")
+    print(test_df)
     
     # split training and testing normalized datasets
     train_df_nomalized = df_nomalized[:-test_window-lookback_window_size] # we leave 100 to have properly calculated indicators
-    test_df_nomalized = df_nomalized[-test_window-lookback_window_size:]
+    test_df_nomalized = df_nomalized[-test_window-lookback_window_size:-720*2]
     # print(train_df)
     # print(train_df_nomalized)
     # time.slepp()
     # #===========================
     # # single processing training
-    # agent = CustomAgent(Actor = 0 , Critic = 0,lookback_window_size=lookback_window_size, lr=0.00001, epochs=5, optimizer=Adam, batch_size = 32, model="CNN", depth=depth, comment="Normalized")
+    agent = CustomAgent(Actor = 0 , Critic = 0,lookback_window_size=lookback_window_size, lr=0.00001, epochs=5, optimizer=Adam, batch_size = 32, model="CNN", depth=depth, comment="Normalized")
     # train_env = CustomEnv(df=train_df, df_normalized=train_df_nomalized, lookback_window_size=lookback_window_size)
     # train_agent(train_env, agent, visualize=False, train_episodes=50000, training_batch_size=500)
-    # test_agent(test_df, test_df_nomalized, visualize=False, test_episodes=10, folder="2021_10_12_04_39_Crypto_trader", name="209141.01_Crypto_trader", comment="3 months", Show_reward=False, Show_indicators=False)
+    test_agent(test_df, test_df_nomalized, visualize=False, test_episodes=10, folder="2021_10_12_14_22_Crypto_trader", name="486726.52_Crypto_trader", comment="3 months", Show_reward=False, Show_indicators=False)
     # agent.load(folder="2021_10_08_07_38_Crypto_trader", name="34259.24_Crypto_trader")
     # # multiprocessing training/testing. Note - run from cmd or terminal
     # Actorr, Criticc = agent.export()
@@ -588,8 +590,8 @@ if __name__ == "__main__":
 
     #=======================================
     # agent = CustomAgent(Actor = Actorr , Critic = Criticc,lookback_window_size=lookback_window_size, lr=0.00001, epochs=5, optimizer=Adam, batch_size=32, model="CNN", depth=depth, comment="Normalized")
-    agent = CustomAgent(Actor = 0 , Critic = 0,lookback_window_size=lookback_window_size, lr=0.00001, epochs=5, optimizer=Adam, batch_size=32, model="CNN", depth=depth, comment="Normalized")
-    train_multiprocessing(CustomEnv, agent, train_df, train_df_nomalized, num_worker = 1, training_batch_size=100, visualize=False, EPISODES=40000)
+    # agent = CustomAgent(Actor = 0 , Critic = 0,lookback_window_size=lookback_window_size, lr=0.00001, epochs=5, optimizer=Adam, batch_size=32, model="CNN", depth=depth, comment="Normalized")
+    # train_multiprocessing(CustomEnv, agent, train_df, train_df_nomalized, num_worker = 1, training_batch_size=100, visualize=False, EPISODES=40000)
 
     # test_multiprocessing(CustomEnv, CustomAgent, test_df, test_df_nomalized, num_worker = 16, visualize=False, test_episodes=1000, folder="2021_02_18_21_48_Crypto_trader", name="3906.52_Crypto_trader", comment="3 months")
     # test_multiprocessing(CustomEnv, CustomAgent, test_df, test_df_nomalized, num_worker = 16, visualize=True, test_episodes=1000, folder="2021_02_21_17_54_Crypto_trader", name="3263.63_Crypto_trader", comment="3 months")
